bc303e881b0f73d59185a38d15fd54bbcc56124a
CZhu <zhu.cathulhu@gmail.com>
2026-01-31 17:53:37 -0500

    startup + fix unused variables

--- NPC/Assets/Scripts/Scene01/Scene01.cs
+++ NPC/Assets/Scripts/Scene01/Scene01.cs
@@ -8,7 +8,6 @@ public class Scene01 : MonoBehaviour
         //need to control the blinkers
         public GameObject BlinkTop;
         public GameObject BlinkBottom;
-        public GameObject BlackScreen;
 
         //run once at start
         void Start()
@@ -21,7 +20,6 @@ public class Scene01 : MonoBehaviour
                 yield return new WaitForSeconds(2);
                 BlinkTop.SetActive(true);
                 BlinkBottom.SetActive(true);
-                BlackScreen.SetActive(false);
         }
 
         //run constantly
